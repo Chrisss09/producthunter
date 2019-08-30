6328ddf409c7d922eda239fd1fa03eba0560ad5e
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-08-30 21:43:18 +0100

    Created code for a user login system

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def signup(request):
-    render(request, 'accounts/signup.html')
+    if request.method == "POST":
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'Username already exists'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Password does not match!!!'})
+    else:
+        return render(request, 'accounts/signup.html')
 
 def login(request):
-    render(request, 'accounts/login.html')
+    return render(request, 'accounts/login.html')
 
 def logout(request):
-    render(request, 'accounts/signup.html')
+    return render(request, 'accounts/signup.html')
