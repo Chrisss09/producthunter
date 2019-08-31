41e7ab5a75536cd51626762bca54101cbfe26a34
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-08-31 17:23:14 +0100

    Added file structure when saving an image

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -121,8 +121,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 STATIC_URL = '/static/'
